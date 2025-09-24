80d96239716103453971c675a25f064f52d663f8
Yiheng Ji <jyh216@login2.ls6.tacc.utexas.edu>
2025-09-24 10:43:43 -0500

    minor sync

--- gr00t/model/action_head/flow_matching_action_head.py
+++ gr00t/model/action_head/flow_matching_action_head.py
@@ -412,11 +412,7 @@ class FlowmatchingActionHead(nn.Module):
         combined_features = torch.cat(
             [vl_embs, obs_features_expanded, lookback_n_expanded], dim=-1
         )  # (B, T, 4097)
-        delta_vlm_embs = self.vlm_obs_fusion(combined_features, embodiment_id)  # (B, T, 2048)
-        vl_embs = vl_embs + delta_vlm_embs
-        print(
-            f"[DEBUG] delta_vlm_embs: range=[{delta_vlm_embs.min().item():.6f}, {delta_vlm_embs.max().item():.6f}]"
-        )
+        vl_embs = self.vlm_obs_fusion(combined_features, embodiment_id)  # (B, T, 2048)
 
         ################### vlm update ###################
         # state_features = self.state_obs_encoder(action_input.state, action_input.simple_img, embodiment_id)
